084a0ac50304f478dbb5ee868e1b4bf83b1f2814
André Carvalho <alac1984@gmail.com>
2023-06-18 12:21:05 -0300

    Changed test logic and conftest fixture

--- repository/grupo.py
+++ repository/grupo.py
@@ -1,8 +1,9 @@
+from typing import Optional
 from sqlalchemy.ext.asyncio import AsyncSession
 from models.grupo import Grupo
 
 
-async def repo_retrieve_grupo(grupo_id: int, session: AsyncSession) -> Grupo:
+async def repo_retrieve_grupo(grupo_id: int, session: AsyncSession) -> Optional[Grupo]:
     result = await session.get(Grupo, grupo_id)
 
     return result
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,36 @@
-import os
 import pytest_asyncio
-from dotenv import load_dotenv
+from sqlmodel import SQLModel
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
-
-
-load_dotenv()
+from models.grupo import Grupo  # noqa
+from models.macroprocesso import Macroprocesso  # noqa
 
 
 @pytest_asyncio.fixture(scope="function")
 async def test_session():
-    DATABASE_URL = os.environ.get("DATABASE_URL")
+    DATABASE_URL = (
+        "postgresql+asyncpg://test_portalprocessos:a1k8u2@localhost:5433"
+        "/test_portalprocessos"
+    )
     engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
     async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
-    async with async_session as session:
+    async with async_session() as session:
+        grupo1 = Grupo(id=1, nome="grupo_teste", nome_exibicao="Grupo de Testes")
+        macroprocesso1 = Macroprocesso(
+            id=1, nome="macro_teste", nome_exibicao="Macro de Testes"
+        )
+        session.add(grupo1)
+        session.add(macroprocesso1)
+        await session.commit()
+
         yield session
+
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.drop_all)
+
+    await engine.dispose()
