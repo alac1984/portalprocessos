28ee4607b55726455140ac586f02366f969df74c
André Carvalho <alac1984@gmail.com>
2023-06-24 17:47:02 -0300

    Issue 19 done

--- application/main.py
+++ application/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
 from api.routes import router as api_router
+from webapp.routes import router as web_router
 
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 app.include_router(api_router, prefix="/api")
+app.include_router(web_router)
