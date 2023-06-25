21186a81fbc4e41036ec128a5c82af9687099a36
André Carvalho <alac1984@gmail.com>
2023-06-25 13:55:12 -0300

    base.html template created

--- webapp/routes.py
+++ webapp/routes.py
@@ -11,4 +11,19 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    dashboard_content = templates.TemplateResponse(
+        "dashboard.html", {"request": request}
+    )
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "default_content": dashboard_content}
+    )
+
+
+@router.get("/layouts", response_class=HTMLResponse)
+async def layouts(request: Request):
+    return templates.TemplateResponse("layouts.html", {"request": request})
+
+
+@router.get("/dashboard", response_class=HTMLResponse)
+async def dashboard(request: Request):
+    return templates.TemplateResponse("dashboard.html", {"request": request})
